aebc4ba2957217fea7fb6df1f69e10e1698c3ed4
mallorymariegordon <32749181+mallorymariegordon@users.noreply.github.com>
2017-10-22 21:37:21 -0500

    Create designcss.css

---
+++ designcss.css
@@ -0,0 +1,101 @@
+header {
+  background-color: #4424D6;
+  padding: 30px 20px;
+}
+
+h1 { font-family: "Keania One", cursive;
+    font-family:
+    color: #34091C;
+    letter-spacing: 1px;
+    font-weight: 400;
+    font-size: 45px;
+    margin: 0;
+}
+
+a {
+	font-family: "Keania One";
+	color: #8601AF;
+	letter-spacing: 2px;
+	font-weight: 600;
+	font-size: 18px;
+  margin: 0 20px;
+  border-bottom: 4px 4px 4px 0 solid;
+  text-decoration: none;
+}
+
+a:hover {
+  border-color:#4424D6;
+}
+
+h2 {
+	font-family: "Keania One";
+	color: #4424D6;
+    letter-spacing: 1px;
+    font-weight: 600;
+    font-size: 20px;
+}
+
+p {
+  font-family: "Fjalla One" sans-serif;
+  line-height: 26px;
+  font-size: 18px;
+}
+
+body {
+    text-align: center;
+    margin: 2px 2px 2px 2px;
+}
+
+nav {
+  background-color: #DAD4F7;
+  padding: 20px 0;
+}
+
+main {
+  width: 850px;
+  margin: 35px auto;
+}
+
+img {
+  margin: 40px 8px;
+}
+
+footer {
+  text-transform: uppercase;
+  padding: 0 20px;
+  background-color: #34091C;
+  color: #DAD4F7;
+  font-size: 14px;
+  letter-spacing: .08em;
+  font-weight: 500;
+}
+
+.copyright {
+  float: left;
+  font-family: "Keania One";
+}
+
+.message {
+  float: right;
+  font-family: "Keania One";
+}
+
+.clearfix:after {
+  visibility: hidden;
+  display: block;
+  content: " ";
+  clear: both;
+  height: 0;
+  font-size: 0;
+}
+
+.name {
+  color: #DAD4F7;
+  font-weight: 700;
+}
+
+.facebook {
+	position: relative;
+	top: 35px;
+	right: 18px;
+}
